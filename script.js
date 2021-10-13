62c7e1389e6e77b9528f045d792994de7fd1ff25
Kcisti <91692567+Kcisti@users.noreply.github.com>
2021-10-13 18:34:12 +0200

    update locator

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+
 function geoFindMe() {
-
   const status = document.querySelector('#status');
-  const mapLink = document.querySelector('#map-link');
 
-  mapLink.textContent = '';
+  if(!navigator.geolocation) {
+    status.textContent = "GEOLOCATION ISN'T SUPPORTED";
+  } else {
+    status.textContent = 'LOCATING...';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 
   function success(position) {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
-    status.textContent = '';
-    var coordinate = mapLink.textContent = `latitude: ${latitude}, longitude: ${longitude}`;
+    status.style.display = 'none';
+    document.getElementById("latitude").textContent = `LATITUDE: ${latitude}`;
+    document.getElementById("longitude").textContent = `LONGITUDE: ${longitude}`;
   }
 
   function error() {
-    status.textContent = 'Unable to retrieve your location';
+    status.textContent = 'ERROR: POSITION NOT FOUND';
   }
 
-  if(!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
-  } else {
-    status.textContent = 'locating...';
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-
+  jQuery.get("https://ipregistry.co", function (data){
+    console.log("COUNTRY:" + data.country);
+    console.log("IP ADRESS:" + data.ip);
+      const country = data.country;
+      const ip = data.ip;
+      document.getElementById("country").textContent = `COUNTRY: ${country}`;
+      document.getElementById("ip").textContent = `IP: ${ip}`;
+  },"jsonp")
 }
